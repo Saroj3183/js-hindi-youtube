2133a245e6e64c8dbd316c499ec0674e30ccb1b5
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-07-15 18:17:41 +0000

    datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using nodejs , not browser
+
+console.log(3 + 3)
+
+//code readability should be high
+
+console.log("saroj")
+
+let name = "saroj" //string datatype
+let age = 21  //integer or number dt
+let isLoggedIn = "true" //boolean
+let state; //undefined
+
+
+// primitives datatypes
+
+//number => size 2 power of 53
+//string => ""
+//bigint => for big number like used in stock market etc.
+//boolean => true/false
+//null => standalone value
+//undefined => when values is not assigned in variable
+//symbol => unique
+
+
+//non-primitives
+
+//object
+//array
+
+console.log(typeof null); //object type
+console.log(typeof undefined); //undefined
+
