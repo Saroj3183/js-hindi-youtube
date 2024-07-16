96721d2e15b5581ceefef8901150d10d6e8da588
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-07-16 18:55:34 +0000

    comparision

---
+++ 01-basics/04-comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); //false   /* reason is that an equity check == and comparision <,>,<=,>=
+// console.log(null == 0); //false     work differently , comparision convert null to a number, treating
+// console.log(null >= 0); //true     it as 0 that is why null >= 0 (true) and null>0 (false)
+
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined = 0);  //false
+
+
+// ===
+console.log("2" === 2); //check datatypes too.
