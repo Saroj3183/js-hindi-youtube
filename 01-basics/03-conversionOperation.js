8410e911bab2dacf80f3de65b86f6856e46c05f6
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-07-16 17:50:22 +0000

    operations

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -25,5 +25,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*********************operation************************* */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// Arithmetic operation
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 ="saroj"
+let str3 = str1 + str2
+//console.log(str3);
+
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((1 + 2) * 3 % 4 );
+
+// console.log(+true); dont use this in code
+// console.log(+"");
+
+let sum1 , sum2 , sum3;
+sum1 = sum2 = sum3 = 2 + 2
+
+// assignment operator
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
