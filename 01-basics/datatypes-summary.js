8548240d0a591e1130f234c3e0c292eaa36eac11
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-07-17 08:20:45 +0000

    memory

--- 01-basics/datatypes-summary.js
+++ 01-basics/datatypes-summary.js
@@ -15,7 +15,7 @@ let userEmail; //undefined
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId); //false
 
 const bigNumber = 345665748078773n
 
@@ -34,4 +34,30 @@ const myFunction = function(){  //function  datatype-function
     console.log("hello world");
 }
 
-console.log(typeof heros);
+//console.log(typeof heros); //object
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack(primitive) , Heap(non primitive)
+// stack - variable declare kri to aeni copy mle 
+/* heap - jyare heap ma object k kai define kri to reference mle original value mle 
+            j b value change kri e original value ma change thay*/
+
+let myYoutubename = "sarojthakordotcom"
+let anotherName = myYoutubename
+
+anotherName = "chaiaurcode"
+
+console.log(anotherName);  //chaiaurcode
+console.log(myYoutubename);  //sarojthakordotcom
+
+let userOne = {
+    Email : "user@gmail.com",
+    Upi :"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.Email = "saroj@313"
+
+console.log(userOne.Email);
+console.log(userTwo.Email);
