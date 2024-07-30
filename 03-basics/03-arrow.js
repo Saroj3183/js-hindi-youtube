15f4956eb2786c7e3b929434f369a4ac07149d36
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-07-30 16:56:54 +0000

    Arrow function

--- 03-basics/03-arrow.js
+++ 03-basics/03-arrow.js
@@ -14,9 +14,39 @@ const user = {
 
 //console.log(this);// {} <= node environment &&& browser => window object (global object)
 
-function chai(){
-     let username = "saroj"
-    console.log(this.username); //undefined , bcoz it only works on object
-}
-chai()
+// function chai(){
+//      let username = "saroj"
+//     //console.log(this.username); //undefined , bcoz this is only works on object
+// }
+// chai()
 
+// const chai = function(){
+//     let username = "saroj"
+//     console.log(this); //global object
+// }
+// chai()
+
+//++++++++++++++++++ Arrow function      +++++++++++++++++++++++++++++++++++++++
+
+// const chai = () => {     //explicit return when we use return keyword 
+//      let username = "saroj"
+//     console.log(this); //{}
+// }
+// chai()
+
+
+// jyare arrow function ma this keyword use kri tyare e output = {}  
+//jyare regular function ma this keyword print kri to output ma = global object
+
+// const addTwo = (num1,num2) => {     explicit return
+//     return num1 + num2               
+// }
+
+// console.log(addTwo(2,3)) //5
+
+// const addTwo = (num1,num2) =>  num1 + num2  //5  implicit return
+
+//const addTwo = (num1,num2) => ( num1 + num2 ) //5
+
+const addTwo = (num1,num2) => ( { username : "saroj"} ) // username : 'saroj'
+console.log(addTwo(2,3))
