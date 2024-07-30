f84b7c8056ab9e9e7ac8ad9f42c437536f7993a4
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-07-30 10:53:58 +0000

    mini hoisting

--- 03-basics/02-scopes.js
+++ 03-basics/02-scopes.js
@@ -9,13 +9,54 @@ let a = 300
 
 
 if (true) {
-    let a =10
+    let a = 10
     const b = 20
     var c = 30
-    console.log("INNER:", a); //inner: 10
-    
+    //console.log("INNER:", a); //inner: 10
+
 }
 
-console.log(a); //output =block scope value a is not defined ,, after declare let 300 in global scope it gives output 300
+//console.log(a); //output =block scope value a is not defined ,, after declare let 300 in global scope it gives output 300
 //console.log(b);//output = b is not defined
 //console.log(c); // 30 , if we declare var 300 in global scope it gives output 30
+
+
+function one() { // scope na andar nu e baar print na karavi shaki pn baar nu andar print kari shaki
+    const username = "saroj"
+
+    function two() {
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website); print ny thay kmk andar thi baar avyu atle
+
+    two()
+}
+ one()
+
+ //nested fun => child (fun. two) function parent(fun. one) k variable ko access kr shakte he
+
+ if (true) {
+    const username = "saroj"
+    
+    if (username=== "saroj") { // we can right true or username === "saroj"
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    // console.log(website); scope ni bahar chhe mate print ny thay
+ }
+//  console.log(username); scope ni baar chhe to print ny thay
+
+//++++++++++++++++++++++++++++  Interesting ++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(5)); //6
+function addOne(num){
+    return num + 1
+
+}
+
+addTwo(5) // it gives error bcoz function k sath variable me store kiya 
+const addTwo = function(num){
+    return num + 2
+}
