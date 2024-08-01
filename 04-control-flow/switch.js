e1b582054a24436d38101ce61d6b934080d01d7b
Saroj3183 <142988950+Saroj3183@users.noreply.github.com>
2024-08-01 18:36:36 +0000

    switch statement

---
+++ 04-control-flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("fab");
+//         break;
+//     case 3:
+//         console.log("march"); // output = march
+//         break;   // break na lagavi to ena pachhi no badho code run thyi print thse default shivay
+//     case 4:
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;   
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
